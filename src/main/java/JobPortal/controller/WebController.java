9e905e1d6772b8c7f03acdf363850a060fc9a840
tjmorrissey <60196631+tjmorrissey@users.noreply.github.com>
2020-04-21 15:13:15 -0500

    Created Job List, and Add Job, will wait on Employer to test

--- src/main/java/JobPortal/controller/WebController.java
+++ src/main/java/JobPortal/controller/WebController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
 import JobPortal.beans.Applicant;
+import JobPortal.beans.Employer;
+import JobPortal.beans.Job;
 import JobPortal.repository.ApplicantRepository;
 import JobPortal.repository.EmployerRepository;
 import JobPortal.repository.JobRepository;
@@ -20,6 +22,11 @@ public class WebController {
 	ApplicantRepository appRepo;
 	EmployerRepository empRepo;
 	JobRepository jobRepo;
+	
+	@GetMapping("/index")
+	public String goToIndex() {
+		return "index";
+	}
 	
 	@GetMapping("/appLogin")
 	public String appLogin(@PathVariable("username") String username, Model model) {
@@ -48,6 +55,32 @@ public class WebController {
 		return appLogin(username, model);
 	}
 	
+	@GetMapping("/addNewJob")
+	public String addNewJob(Model model) {
+		Job j = new Job();
+		model.addAttribute("newJob", j);
+		
+		//will need to send employer id from the Employer Home Page, allowing for company to auto fill.
+		//Employer comp = model.getAttribute("comp");
+		//model.addAttribute("company", comp);
+		
+		return "createNewJob";
+	}
 	
+	@PostMapping("addNewJob")
+	public String addNewJob(@ModelAttribute Job j, Model model) {
+		jobRepo.save(j);
+		
+		//return to company profile, with list of jobs
+		return "index";
+	}
+	
+	@GetMapping("/showJobList")
+	public String showJobList(Model model) {
+		
+		
+		
+		return "jobList";
+	}
 	
 }
