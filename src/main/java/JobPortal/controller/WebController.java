8636afc2084cf6c0502d4d16095e7b8a102c69b3
jakuberski <60200680+jakuberski@users.noreply.github.com>
2020-04-22 17:20:51 -0500

    Fixed the applicant Home Page

--- src/main/java/JobPortal/controller/WebController.java
+++ src/main/java/JobPortal/controller/WebController.java
@@ -15,6 +15,7 @@ import JobPortal.repository.ApplicantRepository;
 import JobPortal.repository.EmployerRepository;
 import JobPortal.repository.JobRepository;
 
+
 @Controller
 public class WebController {
 
@@ -22,65 +23,80 @@ public class WebController {
 	ApplicantRepository appRepo;
 	EmployerRepository empRepo;
 	JobRepository jobRepo;
-	
+
 	@GetMapping("/index")
 	public String goToIndex() {
 		return "index";
 	}
-	
+
 	@GetMapping("/appLogin")
 	public String appLogin(@PathVariable("username") String username, Model model) {
-		
+
 		Applicant app = appRepo.findAppByUsername(username);
-		
+
 		model.addAttribute("applicant", app);
 		return "applicantHomePage";
 	}
-	
-	
+
 	@GetMapping("/addNewApp")
 	public String addNewAppicant(Model model) {
 		Applicant a = new Applicant();
 		model.addAttribute("newApp", a);
-		
+
 		return "createNewApplicant";
 	}
-	
+
 	@PostMapping("addNewApp")
 	public String addNewApplicant(@ModelAttribute Applicant a, Model model) {
 		appRepo.save(a);
-		
+
 		String username = a.getUsername();
-		
+
 		return appLogin(username, model);
 	}
-	
+
 	@GetMapping("/addNewJob")
 	public String addNewJob(Model model) {
 		Job j = new Job();
 		model.addAttribute("newJob", j);
-		
-		//will need to send employer id from the Employer Home Page, allowing for company to auto fill.
-		//Employer comp = model.getAttribute("comp");
-		//model.addAttribute("company", comp);
-		
+
+		// will need to send employer id from the Employer Home Page, allowing for
+		// company to auto fill.
+		// Employer comp = model.getAttribute("comp");
+		// model.addAttribute("company", comp);
+
 		return "createNewJob";
 	}
-	
+
 	@PostMapping("addNewJob")
 	public String addNewJob(@ModelAttribute Job j, Model model) {
 		jobRepo.save(j);
-		
-		//return to company profile, with list of their jobs
+
+		// return to company profile, with list of their jobs
 		return "index";
 	}
-	
+
 	@GetMapping("/showJobList")
 	public String showJobList(Model model) {
-		
+
 		model.addAttribute("jobs", jobRepo.findAll());
-		
+
 		return "jobList";
 	}
-	
+
+	@GetMapping("/edit/{id}")
+	public String showUpdateApplicant(@PathVariable("applicantId") long id, Model model) {
+		Applicant c = appRepo.findById(id).orElse(null);
+		System.out.println("ITEM TO EDIT: " + c.toString());
+		model.addAttribute("newApplicant", c);
+		return "applicantHomePage";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteApplicant(@PathVariable("applicantId") long id, Model model) {
+		Applicant c = appRepo.findById(id).orElse(null);
+		appRepo.delete(c);
+		return "applicantHomePage";
+	}
+
 }
